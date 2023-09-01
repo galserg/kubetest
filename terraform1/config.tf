9a67babd77dc0979ab1deecfeb9d878049c3c4b5
Serhii Haliuzin <serhii.haliuzin@stakelogic.com>
2023-09-01 15:55:51 +0300

    f

--- terraform1/config.tf
+++ terraform1/config.tf
@@ -6,11 +6,11 @@ resource "local_file" "docker_compose" {
 resource "local_file" "master_config" {
   for_each = var.masters_list
   filename = "../config1/${each.key}.yaml"
-  content  = templatefile("controlplane.tftpl", { ipaddr = each.value.ipaddr,  hostname = each.key})
+  content  = templatefile("controlplane.tftpl", { ipaddr = each.value.ipaddr, macaddr = each.value.macaddr, hostname = each.key})
 }
 
 resource "local_file" "workers_config" {
   for_each = var.workers_list
   filename = "../config1/${each.key}.yaml"
-  content  = templatefile("worker.tftpl", { ipaddr = each.value.ipaddr,  hostname = each.key})
+  content  = templatefile("worker.tftpl", { ipaddr = each.value.ipaddr,  macaddr = each.value.macaddr, hostname = each.key})
 }
